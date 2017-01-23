4281c5d1a562866f5fa4524322aa2519920fc174
Chris Wilhelm <clwilhelm@student.ysu.edu>
2017-01-23 13:14:09 -0500

    fixing repo and updating sts project

---
+++ TwitchFollowerSuggestion/src/main/java/com/ollee/TwitchFollowerSuggestionApplication.java
@@ -0,0 +1,12 @@
+package com.ollee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchFollowerSuggestionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchFollowerSuggestionApplication.class, args);
+	}
+}
