f706502e11d4a0c519b8b9db8a01745b25417ffb
ollee <ollee64@gmail.com>
2017-02-10 17:33:55 -0500

    Cleaning repo, found out the API I was using is actually broken is a really inconsistent way. Rebuilding off repo public library tonight and tomorrow and updating the public library repo to work correctly(all of the available API libraries for java are broken right now).

---
+++ TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchApiTest.java
@@ -0,0 +1,14 @@
+package TwitchApiTest;
+
+import java.util.List;
+
+// client id: 5uuaaq02y9fv6j7phunakg0uoo5xu71
+
+public class TwitchApiTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TwitchWrapper twitch = new TwitchWrapper("5uuaaq02y9fv6j7phunakg0uoo5xu71");
+
+	}
+}
---
+++ TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchWrapper.java
@@ -0,0 +1,40 @@
+package TwitchApiTest;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
+import me.philippheuer.twitch4j.*;
+import me.philippheuer.twitch4j.endpoints.ChannelEndpoint;
+import me.philippheuer.twitch4j.model.User;
+
+public class TwitchWrapper {
+	private String ClientID;
+
+	public TwitchWrapper(String c) {
+		this.setClientID(c);
+		this.build();
+	}
+
+	private void build(){
+		TwitchClient twitch = new TwitchClient(this.getClientID(), "e17btqxk3f85piyf66zvlbmwfi5oc2");
+		ChannelEndpoint channelEndpoint;
+		Optional<Long> channelId = twitch.getUserEndpoint().getUserIdByUserName("flamegoat");
+		
+		if(channelId.isPresent()){
+			channelEndpoint = twitch.getChannelEndpoint(channelId.get());
+		}else{
+			System.out.println("Error getting userID");
+		}
+		
+		
+	}
+	public void setClientID(String clientID) {
+		ClientID = clientID;
+	}
+
+	public String getClientID() {
+		return ClientID;
+	}
+	
+}
