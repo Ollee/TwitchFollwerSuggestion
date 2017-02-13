193880127197bef66893258af44f09e6938ac03c
ollee <ollee64@gmail.com>
2017-02-13 10:23:43 -0500

    updated usage of datatypes so getChannelFollowers works right

--- TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchApiTest.java
+++ TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchApiTest.java
@@ -12,11 +12,17 @@ public class TwitchApiTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		TwitchWrapper twitch = new TwitchWrapper();
-		List<Follow> follows = twitch.getChannelFollowers("ollee64");
-		
+		List<Follow> follows = twitch.getChannelFollowers("leshkee");
+		System.out.println("Exited getChannelFollowers with follows.size() " + follows.size());
 		Iterator<Follow> followsIterator = follows.iterator();
+		System.out.println("Iterator created");
 		while(followsIterator.hasNext()){
-			System.out.print(followsIterator.next().getUser().getDisplayName());
+//			System.out.println("follows iterator loop");
+			System.out.println(followsIterator.next().getUser().getDisplayName());
 		}
+		
+//		for(int i=0; i < follows.size(); i++){
+//			System.out.println((i+1) + ": " + follows.get(i).getUser().getDisplayName());
+//		}
 	}
 }
--- TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchWrapper.java
+++ TwitchApiTest/TwitchApiTest/src/main/java/TwitchApiTest/TwitchWrapper.java
@@ -1,15 +1,12 @@
 package TwitchApiTest;
 
-import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
-import me.philippheuer.twitch4j.*;
+import me.philippheuer.twitch4j.TwitchClient;
 import me.philippheuer.twitch4j.endpoints.ChannelEndpoint;
-import me.philippheuer.twitch4j.endpoints.UserEndpoint;
 import me.philippheuer.twitch4j.model.Channel;
 import me.philippheuer.twitch4j.model.Follow;
-import me.philippheuer.twitch4j.model.User;
 
 public class TwitchWrapper {
 	private String clientID = "5uuaaq02y9fv6j7phunakg0uoo5xu71";
@@ -17,41 +14,20 @@ public class TwitchWrapper {
 	private TwitchClient twitchClient;
 	
 	public TwitchWrapper() {
-		twitchClient = new TwitchClient(this.getClientID(), this.getClientSecret());
+		try{
+			twitchClient = new TwitchClient(this.getClientID(), this.getClientSecret());
+			}
+		catch (Exception e){
+			System.out.println("Exception caught from TwitchClient: " + e.toString());
+			e.printStackTrace();
+			}
 	}
 
-	private void build(){
-		TwitchClient twitch = new TwitchClient(this.getClientID(), this.getClientSecret());
-		ChannelEndpoint channelEndpoint;
-		int followerCount;
-		Optional<Long> channelId = twitch.getUserEndpoint().getUserIdByUserName("ollee64");
-		
-		
-		channelEndpoint = twitch.getChannelEndpoint(channelId.get());
-		followerCount = channelEndpoint.getChannel().getFollowers();
-		
-		System.out.println("follower count of channel: " + channelEndpoint.getChannel().getDisplayName() + " has followerCount: " + followerCount);
-		
-		List<Follow> follows = channelEndpoint.getFollowers(Optional.ofNullable(followerCount), Optional.empty());
-		System.out.println("followers list contans items n= " + follows.size());
-		
-		Iterator<Follow> followsIterator = follows.iterator();
-		while(followsIterator.hasNext()){
-			System.out.print(followsIterator.next().getUser().getDisplayName());
-		}
-	}
-
-	public String getClientID() {
-		return this.clientID;
-	}
-	private String getClientSecret() {
-		return this.clientSecret;
-	}
 	public List<Follow> getChannelFollowers(String channel){
 		
 		Optional<Long> channelId = twitchClient.getUserEndpoint().getUserIdByUserName(channel);
 		ChannelEndpoint channelEndpoint = twitchClient.getChannelEndpoint(channelId.get());
-		int followerCount = channelEndpoint.getChannel().getFollowers();
+		int followerCount = Math.toIntExact(channelEndpoint.getChannel().getFollowers());
 		
 		System.out.println("The channel: " + channelEndpoint.getChannel().getDisplayName() + " has followerCount: " + followerCount);
 		
@@ -61,6 +37,15 @@ public class TwitchWrapper {
 		
 		return follows;
 	}
+	
+	public String getClientID() {
+		return this.clientID;
+	}
+	
+	private String getClientSecret() {
+		return this.clientSecret;
+	}
+	
 	public List<Channel> getUserChannelsFollowed(String user){
 		Optional<Long> userId = twitchClient.getUserEndpoint().getUserIdByUserName(user);
 		//TODO build getUserChannelsFollows
@@ -73,3 +58,25 @@ public class TwitchWrapper {
 
 	
 }
+
+//old code
+//	private void build(){
+//		TwitchClient twitch = new TwitchClient(this.getClientID(), this.getClientSecret());
+//		ChannelEndpoint channelEndpoint;
+//		int followerCount;
+//		Optional<Long> channelId = twitch.getUserEndpoint().getUserIdByUserName("ollee64");
+//		
+//		
+//		channelEndpoint = twitch.getChannelEndpoint(channelId.get());
+//		followerCount = channelEndpoint.getChannel().getFollowers();
+//		
+//		System.out.println("follower count of channel: " + channelEndpoint.getChannel().getDisplayName() + " has followerCount: " + followerCount);
+//		
+//		List<Follow> follows = channelEndpoint.getFollowers(Optional.ofNullable(followerCount), Optional.empty());
+//		System.out.println("followers list contans items n= " + follows.size());
+//		
+//		Iterator<Follow> followsIterator = follows.iterator();
+//		while(followsIterator.hasNext()){
+//			System.out.print(followsIterator.next().getUser().getDisplayName());
+//		}
+//	}
